7799b5cff5f23e85878ab67ee2cc3ab498af8a82
au-iris <au.iris.30@gmail.com>
2025-12-06 12:18:12 -0500

    update basename config

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,11 +1,8 @@
 import type { Config } from "@react-router/dev/config";
 
-const isInPipeline = process.env.CI === "true" || process.env.NODE_ENV === "production";
-const base = isInPipeline ? '/culturalgold/' : '/';
-console.log("base react-router.config:", base);
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
-  basename: base,
+  basename: '/culturalgold/'
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-const isInPipeline = process.env.CI === "true" || process.env.NODE_ENV === "production";
-const base = isInPipeline ? '/culturalgold/' : '/';
-console.log("base vite.config:", base);
-
 export default defineConfig(({ mode }) => ({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  basename: base
+  basename: '/culturalgold/'
 }));
